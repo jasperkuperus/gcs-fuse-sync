5b71c141ad99aa7a9a026fe086162c482a5769e0
Jasper Kuperus <github@jasperkuperus.nl>
2020-03-14 17:29:50 +0100

    Add a poll interval

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+if [[ -z "${POLL_INTERVAL}" ]]
+then
+  POLL_INTERVAL=10
+fi
+
 gcsfuse --key-file ${KEY_FILE} ${GCS_BUCKET} /gcs-mount
-unison -repeat watch -batch /gcs-mount /bucket-share
+unison -repeat $POLL_INTERVAL -batch /gcs-mount /bucket-share
